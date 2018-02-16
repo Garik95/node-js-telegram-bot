1dfac0602ffdb53f610dab367ab129cf3d4e787f
Garik Nurmurodoff <garikn95@gmail.com>
2018-02-16 10:38:47 -0500

    Bot basic functionality

--- index.js
+++ index.js
@@ -1,54 +1,47 @@
 const TeleBot = require('telebot');
+const mysql = require('mysql');
 
-// var mysql = require('mysql');
+const con = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '123456',
+  database: 'test'
+});
+
 
 const bot = new TeleBot({
     token: '209376928:AAG9Ohui0myFbuvgZpDparxDQgkBEreScpE', // Required. Telegram Bot API token.
-    polling: {
+    polling: { // Optional. Use polling.
         proxy: 'http://10.20.0.109:3128' // Optional. An HTTP proxy to be used.
-    },
+    }
 });
 
-// var con = mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: "123456",
-//   database: "test"
-// });
 
-bot.on('text', (msg) => {
-var res = 1;
-	if(!con) con.connect();
-	
-	function extract(callback){	
-		con.query("SELECT * FROM test_tbl", function (err, result, fields) {
-                        if (err) throw err;
-                        console.log(result[0].name);
-			res = result[0].name;                       
-                	callback(null,res);        
-		});
-	}
-//if (!con)
-		/*con.connect(function(err) {
- 			if (err) throw err;
-  			console.log("Connected!");
-			con.query("SELECT * FROM test_tbl", function (err, result, fields) {
-   			if (err) throw err;
-    			res = result[0].name;
-			//console.log(result);
-  			});
-		});*/
-//con.destroy();
-	console.log(res);
-	
-	function hand(err,result){
-		msg.reply.text(result);
-	};
-	extract(hand);
-	//msg.reply.text(hand);
+function fetchdata(callback,msg){
+	var res;
+	con.query("SELECT * FROM test_tbl", function (err, result, fields) {
+		if (err) throw err;
+		res = result[0].name
+		//console.log(res);
+		callback(null,res,msg);
+	});
+}
+
+
+bot.on(['/start', '/hello'], (msg) => msg.reply.text('Welcome!'));
+
+bot.on('edit', (msg) => {
+    return msg.reply.text('I saw it! You edited message!', { asReply: true });
 });
 
-//con.end();
-//console.log("Disconnected1!");
+//bot.on('text', (msg) => {
+//	
+//	function extract(err,result,msg){
+//		msg.reply.text(result)
+//	}
+//
+//	fetchdata(extract,msg);
+//
+//	});
 
 bot.start();
